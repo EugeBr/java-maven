2bc70a71896055a3aedd9662f76a92597df15990
EugeBr <eugebordenave@gmail.com>
2023-04-18 12:49:03 +0200

    fixed typo

--- src/test/java/com/ironhack/w2/d2/PaymentProcessorTest.java
+++ src/test/java/com/ironhack/w2/d2/PaymentProcessorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PaymentProcessorTests {
+public class PaymentProcessorTest {
 
     private Account buyer;
     private Account seller;
